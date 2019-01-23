f3489b35dbf42f9db8c12f4e06f72cfb17f8d5d6
YSyongsheng <736673717@qq.com>
2019-01-23 23:24:58 +0800

    django-update

--- day02/project/myApp/urls.py
+++ day02/project/myApp/urls.py
@@ -4,6 +4,12 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^students/$', views.students),
+    url(r'^students2/$', views.students2),
+    url(r'^stu/(\d+)/$', views.stupage),
+    url(r'^studentsearch/$', views.studentsearch),
+
+
+
     url(r'^addstudents/$', views.addstudents),
     url(r'^addstudents2/$', views.addstudents2),
 ]
--- day02/project/myApp/views.py
+++ day02/project/myApp/views.py
@@ -13,6 +13,34 @@ def students(request):
     studentsList = Students.stuObj2.all()
     return render(request,'myApp/students.html',{'students':studentsList})
 
+def students2(request):
+    # 如果没有找到符合条件的对象，会引发"模型类.DoesNotExist"异常
+    # 如果找到多个对象，会引发"模型类.MultipleObjectsReturned"异常
+    # 用try处理就可以
+    studentsList = Students.stuObj2.get(sage=55)
+    return HttpResponse("laile")
+
+
+#分页显示学生
+def stupage(request,page):
+    #  1-5   5-10   10-15
+    #   1     2       3
+    #  根据规律算得page
+    page = int(page)
+    studentsList = Students.stuObj2.all()[(page-1)*5:page*5]
+    return render(request, 'myApp/students.html', {'students': studentsList})
+
+# 比较运算符运用之：  查询条件的使用
+def studentsearch(request):
+    # studentsList = Students.stuObj2.filter(sname__contains="李")   # 包含的使用(contains)
+    studentsList = Students.stuObj2.filter(sname__startswith="李")   # 查询以李开头的数据
+    return render(request,'myApp/students.html',{'students':studentsList})
+
+
+
+
+
+
 def addstudents(request):
     grade = Grades.objects.get(pk=1)
     stu = Students.createStudent("李易峰",29,True,grade,"我是李易峰","2019-1-20","2019-1-20")
